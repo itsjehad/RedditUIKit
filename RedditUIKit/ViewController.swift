9f2909343c111c71e0ea7734781a73f6046948dd
amjehadsarkar@route1.com <amjehadsarkar@route1.com>
2019-09-23 17:47:18 -0400

    Initial Commit

---
+++ RedditUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RedditUIKit
+//
+//  Created by Jehad Sarkar on 2019-09-23.
+//  Copyright © 2019 itsjehad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
